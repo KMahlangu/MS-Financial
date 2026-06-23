076064f7d655720fb1bb264436bd30979a905acc
Your Full Name <your.email@saps.gov.za>
2026-06-23 19:09:08 +0200

    Configure Connection String

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,28 @@
-public class ApplicationDbContext : DbContext
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MS.Financial.Web.Models.Entities;
+
+namespace MS.Financial.Web.Data
 {
-    public ApplicationDbContext(DbConetextOptions<ApplicationsDbContext> options)
-        : base(options)
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
 
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<DebtCase> DebtCases { get; set; }
+        public DbSet<CreditReport> CreditReports { get; set; }
+        public DbSet<Document> Documents { get; set; }
+
+        protected override void onModelCreating(ModelBuilder modelbuilder)
+        {
+            base.OnModelCreating(modelbuilder);
+
+            modelbuilder.Entity<Client>()
+                .HasIndex(c => c.IdentityNumber)
+                .IsUnique();
+        }
     }
-
-    public DbSet<Client> Clients { get; set; }
-    public DbSet<DebtCase> DebtCases { get; set; }
-    public DbSet<CreditReport> CreditReports { get; set; }
-    public DbSet<Document> Documents { get; set; }
 }
