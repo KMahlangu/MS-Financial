fa51fe2d82868d1cac52055d4dcaf85c1f1b7d3b
Your Full Name <your.email@saps.gov.za>
2026-06-23 15:50:55 +0200

    Initial project structure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MS.Financial.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
